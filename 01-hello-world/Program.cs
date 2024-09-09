c2548b502fee7a6e513aa3f120c1c6316e6a4ad5
Daniel BLAHOVEC <blahda@delta-studenti.cz>
2024-09-09 12:21:19 +0200

    aaaa

---
+++ 01-hello-world/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
