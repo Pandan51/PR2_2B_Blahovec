5bdc50dc783794b095e8bd64a0fcf93326fd9090
Daniel BLAHOVEC <blahda@delta-studenti.cz>
2024-09-30 12:40:20 +0200

    Test opakování

---
+++ 00-Rev-01-Pod limitem/Pod limitem/PrumerPodLimitem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pod_limitem
+{
+    internal class PrumerPodLimitem
+    {
+        public int[] Nums;
+        public int Limit;
+
+        public PrumerPodLimitem(int[] cisla)
+        {
+            Nums = cisla;
+        }
+
+
+    }
+}
---
+++ 00-Rev-01-pred-maximem/00-Rev-01-pred-maximem/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.Design;
+
+namespace _00_Rev_01_pred_maximem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            int biggest = 0;
+            int before = -999;
+            bool first = true;
+            bool end = false;
+            
+            int last_num = 0;
+
+            
+
+            while (x != "dost")
+            {
+                Console.WriteLine("Piš celá čísla");
+                x = Console.ReadLine();
+
+                if(x == "dost" && first == true)
+                {
+                    end = true;
+                }
+                else if(x == "dost")
+                {
+                    break;
+                }
+
+                if (first)
+                {
+                    try
+                    {
+                        biggest = int.Parse(x);
+                        first = false;
+                        last_num = int.Parse(x);
+                        
+
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        if (biggest < int.Parse(x))
+                        {
+                            before = last_num;
+                            biggest = int.Parse(x);
+                            
+                        }
+                        last_num = int.Parse(x);
+
+
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                
+               
+            }
+            if(end == true)
+            {
+                Console.WriteLine("Nebylo vloženo žádné číslo");
+            }
+            else if(before == -999)
+            {
+                Console.WriteLine(last_num);
+            }
+            else 
+            {
+            Console.WriteLine(before);
+            }
+        }
+
+    }
+}
+    
