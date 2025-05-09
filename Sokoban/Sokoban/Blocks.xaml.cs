d621a68c54fd57a16830d00f90ce85bda633b1e2
Daniel BLAHOVEC <blahda@delta-studenti.cz>
2025-05-09 18:12:27 +0200

    save

---
+++ Sokoban/Sokoban/Blocks.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sokoban
+{
+    //enum GroundType { Wall, Ground, Goal}
+    public enum Occupied {Empty, Player, Box}
+
+    /// <summary>
+    /// Interaction logic for Blocks.xaml
+    /// </summary>
+    public abstract partial class Blocks : UserControl
+    {
+        //private GroundType _groundType;
+        //private Occupied _occupied;
+
+        public virtual bool isWalkable => true;
+
+
+        public Blocks()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class WallBlock : Blocks
+    {
+        public override bool isWalkable => false;
+    }
+
+    public class GroundBlock : Blocks
+    {
+        private Occupied _content;
+        public override bool isWalkable
+        {
+            get
+            {
+                if(content == Occupied.Empty)
+                {
+                    return true;
+                }
+                return false;
+            }
+            //TODO - implementace box logiky
+        }
+        
+        // content - empty, player, box
+        public virtual Occupied content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                _content = value;
+            }
+        }
+
+        
+    }
+
+    public class GoalBlock : GroundBlock
+    {
+        //zatím stejné
+    }
+
+}
---
+++ Sokoban/Sokoban/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sokoban
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+
+
+
+        public int _characterX
+        {
+            get { return (int)GetValue(_characterXProperty); }
+            set { SetValue(_characterXProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for _characterX.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty _characterXProperty =
+            DependencyProperty.Register("_characterX", typeof(int), typeof(MainWindow), new PropertyMetadata(2));
+
+
+
+        public int _characterY
+        {
+            get { return (int)GetValue(_characterYProperty); }
+            set { SetValue(_characterYProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for _characterY.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty _characterYProperty =
+            DependencyProperty.Register("_characterY", typeof(int), typeof(MainWindow), new PropertyMetadata(2));
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_MoveUp(object sender, RoutedEventArgs e)
+        {
+            _characterY--;
+
+        }
+
+        private void Button_MoveLeft(object sender, RoutedEventArgs e)
+        {
+            _characterX--;
+        }
+
+        private void Button_MoveDown(object sender, RoutedEventArgs e)
+        {
+            _characterY++;
+        }
+
+        private void Button_MoveRight(object sender, RoutedEventArgs e)
+        {
+            _characterX++;
+        }
+    }
+}
